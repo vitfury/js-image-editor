5faa752a0c4cb833e53b7bc3a15e32f196e646ee
Artem Murenko <Murenko777@gmail.com>
2020-08-16 18:49:34 +0300

    change erase-circlepoint to center

--- src/js/component/erase.js
+++ src/js/component/erase.js
@@ -45,22 +45,10 @@ class Erase extends Component {
             'mouse:down': this._listeners.mousedown
         });
     }
-
-    setBrush(setting) {
-        const brush = this.getCanvas().freeDrawingBrush;
-
-        setting = setting || {};
-        this.width = setting.width || this.width;
-        if (setting.color) {
-            this.oColor = new fabric.Color(setting.color);
-        }
-        brush.width = this.width;
-        brush.color = this.oColor.toRgba();
-    }
-
     /**
      * End input text mode
      */
+    
     end() {
         const canvas = this.getCanvas();
         canvas.selection = true;
@@ -79,7 +67,7 @@ class Erase extends Component {
     add(options) {
         return new Promise(resolve => {
             const canvas = this.getCanvas();
-            const newPath = new fabric.Path('M100 40C195 40 195 180 100 180M100 180C5 180 5 40 100 40');
+            const newPath = new fabric.Path('M9.141 19.426c-1.923-.142-3.715-.83-5.236-2.008-.561-.435-1.2-1.072-1.648-1.64-.966-1.223-1.628-2.687-1.903-4.203-.108-.591-.141-.925-.152-1.574-.015-.81.046-1.462.201-2.193.591-2.766 2.381-5.139 4.892-6.486.951-.509 2.054-.872 3.11-1.025.545-.079.727-.09 1.399-.092.656 0 .789.008 1.306.075 1.696.219 3.339.92 4.7 2.003.432 .344.964 .858 1.324 1.277 1.17 1.363 1.954 3.082 2.209 4.848.079 .538.09 .722.09 1.412 0 .805-.036 1.213-.172 1.89-.314 1.579-1.016 3.036-2.077 4.313-.223.268-.9.944-1.17 1.169-1.614 1.339-3.465 2.085-5.548 2.236-.264.018-1.061.018-1.326-.002z');
             newPath.set({
                 nameType: 'eraser',
                 left: options.position.x,
